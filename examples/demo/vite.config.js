f2cafc5e47a5c54b34353baaf4f429cf4a0e35da
sidelaw <muhammadsalmansarwar32@gmail.com>
2026-03-30 07:12:30 +0500

    vercel error fix

--- examples/demo/vite.config.js
+++ examples/demo/vite.config.js
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    dedupe: ['react', 'react-dom'],
-    alias: {
-      react: path.resolve('./node_modules/react'),
-      'react-dom': path.resolve('./node_modules/react-dom'),
-    },
-  },
   server: {
     proxy: {
       '/api/fiber-rpc': {
